5ecd373a5980f283e45c520fcbf5e14eb1966f86
Jerome Udoh <admin@jubass.com>
2023-02-07 08:17:10 -0800

    Write a function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,17 +5,3 @@
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
-
-
-vi 1-my_list.py
-
-#!/usr/bin/python3
-"""Defines an inherited list class MyList."""
-
-
-class MyList(list):
-    """Implements sorted printing for the built-in list class."""
-
-    def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
